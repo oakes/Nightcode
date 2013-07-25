c2069c0c426e3623da9fe63360084be072c011ee
oakes <zsoakes@gmail.com>
2013-07-25 07:37:30 -0400

    Update comments in android-java project.clj

--- src/clojure/leiningen/new/android_java/project.clj
+++ src/clojure/leiningen/new/android_java/project.clj
@@ -1,6 +1,5 @@
 (defproject {{raw-name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  ;:resource-paths ["libs/android-support-v13.jar"]
   :java-source-paths ["src" "gen"]
   :java-only true
   :profiles {:dev {:android {:aot :all-with-unused}}
@@ -16,7 +15,41 @@
             ;; here or in your ~/.lein/profiles.clj file.
             ;; :sdk-path "/home/user/path/to/android-sdk/"
 
-            ;; Uncomment this if dexer fails with OutOfMemoryException
+            ;; Specify this if your project is a library.
+            ;; :library false
+
+            ;; Uncomment this if dexer fails with OutOfMemoryException.
             ;; :force-dex-optimize true
 
+            ;; Options to pass to dx executable, former is for general
+            ;; java-related options and later is for 'dex task'-specific options.
+            ;; :dex-opts ["-JXmx4096M"]
+            ;; :dex-aux-opts ["--num-threads=2"]
+
+            ;; Proguard config for "droid create-obfuscated-dex" task.
+            ;; :proguard-conf-path "proguard.cfg"
+            ;; :proguard-opts ["-printseeds"]
+
+            ;; Uncomment this line to be able to use Google API.
+            ;; :use-google-api true
+
+            ;; Use this property to add project dependencies.
+            ;; :project-dependencies [ "/path/to/library/project" ]
+
+            ;; Sequence of external jars or class folders to include
+            ;; into project, e.g. android-support-v4.jar
+            ;; :external-classes-paths ["<sdk-home>/extras/android/support/v4/android-support-v4.jar"
+            ;;                          "path/to/classfiles/"]
+
+            ;; sequence of paths where native libraries may be found for packaging
+            ;; :native-libraries-paths ["libs"]
+
+            ;; Minimum supported version could be specified as well,
+            ;; its meaning is similar to that in AndroidManifest.xml.
+            ;; :min-version "10"
+
+            ;; Sequence of namespaces that should not be compiled.
+            ;:aot-exclude-ns ["clojure.parallel" "clojure.core.reducers"]
+
+            ;; Target version affects api used for compilation.
             :target-version "15"})
