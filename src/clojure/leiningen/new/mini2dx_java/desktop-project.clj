a6881daa1cf1d3c4676b12a1c177fd8ce5fe8462
oakes <zsoakes@gmail.com>
2013-08-09 19:33:22 -0400

    Use sonatype repository

--- src/clojure/leiningen/new/mini2dx_java/desktop-project.clj
+++ src/clojure/leiningen/new/mini2dx_java/desktop-project.clj
@@ -1,11 +1,11 @@
 (defproject {{raw-name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[com.badlogic.gdx/gdx "0.9.9-SNAPSHOT"]
-                 [com.badlogic.gdx/gdx-backend-lwjgl "0.9.9-SNAPSHOT"]
+  :dependencies [[com.badlogicgames.gdx/gdx "0.9.9-SNAPSHOT"]
+                 [com.badlogicgames.gdx/gdx-backend-android "0.9.9-SNAPSHOT"]
                  [org.mini2Dx/mini2Dx-core "0.8"]
                  [org.mini2Dx/mini2Dx-tiled "0.8"]
                  [org.mini2Dx/mini2Dx-dependency-injection "0.8"]]
-  :repositories [["libgdx" "http://libgdx.badlogicgames.com/nightlies/maven/"]
+  :repositories [["sonatype" "https://oss.sonatype.org/content/repositories/snapshots/"]
                  ["mini2Dx-thirdparty"
                   "http://mini2dx.org/nexus/content/repositories/thirdparty"]
                  ["mini2Dx"
