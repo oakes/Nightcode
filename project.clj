725f5682a35865952540b5d07d5afa8136a6da93
oakes <zsoakes@gmail.com>
2013-08-09 23:33:11 -0400

    Revert back the lein 2.2.0 for now

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
             :url "http://unlicense.org/UNLICENSE"}
   :dependencies [[com.github.insubstantial/substance "7.1"]
                  [com.fifesoft/rsyntaxtextarea "2.0.6"]
-                 [leiningen "2.3.0"]
+                 [leiningen "2.2.0"]
                  [lein-cljsbuild "0.3.2"]
                  [lein-droid "0.2.0-SNAPSHOT"]
                  [org.clojure/clojure "1.5.1"]
