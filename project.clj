a5b697a106630d5d707dc8fb8cd9bba091e6dff3
oakes <zsoakes@gmail.com>
2013-08-23 22:53:00 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nightcode "0.0.9"
+(defproject nightcode "0.0.10"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
   :dependencies [[com.github.insubstantial/substance "7.1"]
