ed1caf2245112a9fedd8bba691b155194a20878e
oakes <zsoakes@gmail.com>
2013-08-28 21:54:00 -0400

    Update library versions

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject nightcode "0.0.10"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[com.github.insubstantial/substance "7.1"]
-                 [com.fifesoft/rsyntaxtextarea "2.0.6"]
-                 [leiningen "2.3.1"]
+  :dependencies [[com.github.insubstantial/substance "7.2.1"]
+                 [com.fifesoft/rsyntaxtextarea "2.0.7"]
+                 [leiningen "2.3.2"]
                  [lein-cljsbuild "0.3.2"]
                  [lein-droid "0.2.0-beta2"]
                  [org.apache.bcel/bcel "5.2"]
                  [org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.incubator "0.1.1"]
+                 [org.clojure/core.incubator "0.1.3"]
                  [org.flatland/ordered "1.5.1"]
                  [org.lpetit/net.cgrand.parsley "0.0.13.STABLE002"]
                  [org.lpetit/net.cgrand.regex "0.0.4.STABLE001"]
