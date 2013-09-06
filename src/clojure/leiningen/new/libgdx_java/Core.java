b6e1644a3d20641967095bea146fe338a9517ca8
oakes <zsoakes@gmail.com>
2013-09-05 22:25:33 -0400

    Improve formatting of Java templates

--- src/clojure/leiningen/new/libgdx_java/Core.java
+++ src/clojure/leiningen/new/libgdx_java/Core.java
@@ -5,23 +5,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
 
 public class {{class-name}} implements ApplicationListener {
-	public void create () {
+	public void create() {
 	}
 
-	public void render () {
+	public void render() {
 		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 	}
 
-	public void resize (int width, int height) {
+	public void resize(int width, int height) {
 	}
 
-	public void pause () {
+	public void pause() {
 	}
 
-	public void resume () {
+	public void resume() {
 	}
 
-	public void dispose () {
+	public void dispose() {
 	}
 }
--- src/clojure/leiningen/new/mini2dx_java/Core.java
+++ src/clojure/leiningen/new/mini2dx_java/Core.java
@@ -4,12 +4,15 @@ import org.mini2Dx.core.game.BasicGame;
 import org.mini2Dx.core.graphics.Graphics;
 
 public class {{class-name}} extends BasicGame {
-	public void initialise () {
+	public void initialise() {
 	}
 
-	public void update (float delta) {
+	public void update(float delta) {
 	}
 
-	public void render (Graphics g) {
+	public void interpolate(float alpha) {
+	}
+
+	public void render(Graphics g) {
 	}
 }
