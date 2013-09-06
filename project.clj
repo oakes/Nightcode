5fface847d41f1be06b6e420e7ef91450cd4e4d2
oakes <zsoakes@gmail.com>
2013-09-05 22:25:54 -0400

    Update lein-droid

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [com.fifesoft/rsyntaxtextarea "2.0.7"]
                  [leiningen "2.3.1"]
                  [lein-cljsbuild "0.3.2"]
-                 [lein-droid "0.2.0-beta2"]
+                 [lein-droid "0.2.0-beta3"]
                  [org.apache.bcel/bcel "5.2"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/core.incubator "0.1.3"]
