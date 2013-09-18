341dce06aee758d408eb6220289b5511af48f721
oakes <zsoakes@gmail.com>
2013-09-18 13:48:27 -0400

    Fix bugs in Clojure's AndroidLauncher

--- src/clojure/leiningen/new/libgdx_clojure/android-launcher.clj
+++ src/clojure/leiningen/new/libgdx_clojure/android-launcher.clj
@@ -4,7 +4,7 @@
 (gen-class
   :name {{package}}.{{activity}}
   :extends com.badlogic.gdx.backends.android.AndroidApplication
-  :exposes-methods [superOnCreate onCreate])
+  :exposes-methods {superOnCreate onCreate})
 (defn -onCreate [this saved-instance-state]
-  (superOnCreate this saved-instance-state)
+  (.superOnCreate this saved-instance-state)
   (.initialize this ({{namespace}}.Game.) false))
