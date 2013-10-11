7a32a6e1a19c87a6032b8be107424df6419d0118
oakes <zsoakes@gmail.com>
2013-10-11 14:17:46 -0400

    Make Clojure LibGDX code more idiomatic

--- src/clojure/leiningen/new/libgdx_clojure/core.clj
+++ src/clojure/leiningen/new/libgdx_clojure/core.clj
@@ -5,26 +5,25 @@
            [com.badlogic.gdx.scenes.scene2d Stage]
            [com.badlogic.gdx.scenes.scene2d.ui Label Label$LabelStyle]))
 
-(declare ^Stage stage)
-
 (def main-screen
-  (proxy [Screen] []
-    (show []
-      (def stage (Stage.))
-      (let [style (Label$LabelStyle. (BitmapFont.) (Color. 1.0 1.0 1.0 1.0))
-            label (Label. "Hello world!" style)]
-        (.addActor stage label)))
-    (render [delta]
-      (.glClearColor (Gdx/gl) 0 0 0 0)
-      (.glClear (Gdx/gl) GL20/GL_COLOR_BUFFER_BIT)
-      (doto stage
-        (.act delta)
-        (.draw)))
-    (dispose[])
-    (hide [])
-    (pause [])
-    (resize [w h])
-    (resume [])))
+  (let [stage (atom nil)]
+    (proxy [Screen] []
+      (show []
+        (reset! stage (Stage.))
+        (let [style (Label$LabelStyle. (BitmapFont.) (Color. 1 1 1 1))
+              label (Label. "Hello world!" style)]
+          (.addActor @stage label)))
+      (render [delta]
+        (.glClearColor (Gdx/gl) 0 0 0 0)
+        (.glClear (Gdx/gl) GL20/GL_COLOR_BUFFER_BIT)
+        (doto @stage
+          (.act delta)
+          (.draw)))
+      (dispose [])
+      (hide [])
+      (pause [])
+      (resize [w h])
+      (resume []))))
 
 (gen-class
   :name {{namespace}}.Game
