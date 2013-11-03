e29ede8bfbf94dbf8cffffdc1e438bba7fc43861
oakes <zsoakes@gmail.com>
2013-11-03 18:16:41 -0500

    Update ClojureScript

--- src/clojure/leiningen/new/web_clojure/project.clj
+++ src/clojure/leiningen/new/web_clojure/project.clj
@@ -1,7 +1,7 @@
 (defproject {{name}} "0.0.1-SNAPSHOT"
   :description "FIXME: write this!"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1934"]
+                 [org.clojure/clojurescript "0.0-1978"]
                  [ring "1.1.8"]]
   :plugins [[lein-cljsbuild "0.3.3"]
             [lein-ring "0.8.5"]]
