601ef6857c254b5f4bf5b3b7219752efcddad45d
oakes <zsoakes@gmail.com>
2013-11-13 17:50:34 -0500

    Simplify java game template

--- src/clojure/leiningen/new/game_java/MainScreen.java
+++ src/clojure/leiningen/new/game_java/MainScreen.java
@@ -1,16 +1,13 @@
 package {{package}};
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Graphics;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 
 public class {{screen-class-name}} implements Screen {
-	private Stage stage;
+	Stage stage;
 	
 	public void show() {
 		stage = new Stage();
@@ -22,6 +19,7 @@ public class {{screen-class-name}} implements Screen {
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		stage.act(delta);
 		stage.draw();
 	}
