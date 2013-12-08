4f27489f719758a70264e5c48b7cd7d3745b7a49
oakes <zsoakes@gmail.com>
2013-12-08 18:19:24 -0500

    Break out the show/render methods into funtions so Refresh redefines them

--- src/clojure/leiningen/new/game_clojure/core.clj
+++ src/clojure/leiningen/new/game_clojure/core.clj
@@ -5,20 +5,25 @@
            [com.badlogic.gdx.scenes.scene2d Stage]
            [com.badlogic.gdx.scenes.scene2d.ui Label Label$LabelStyle]))
 
+(defn show
+  [stage]
+  (let [style (Label$LabelStyle. (BitmapFont.) (Color. 1 1 1 1))
+        label (Label. "Hello world!" style)]
+    (.addActor stage label)))
+
+(defn render
+  [stage delta]
+  (.glClearColor (Gdx/gl) 0 0 0 0)
+  (.glClear (Gdx/gl) GL20/GL_COLOR_BUFFER_BIT)
+  (doto stage
+    (.act delta)
+    (.draw)))
+
 (def main-screen
   (let [stage (atom nil)]
     (proxy [Screen] []
-      (show []
-        (reset! stage (Stage.))
-        (let [style (Label$LabelStyle. (BitmapFont.) (Color. 1 1 1 1))
-              label (Label. "Hello world!" style)]
-          (.addActor @stage label)))
-      (render [delta]
-        (.glClearColor (Gdx/gl) 0 0 0 0)
-        (.glClear (Gdx/gl) GL20/GL_COLOR_BUFFER_BIT)
-        (doto @stage
-          (.act delta)
-          (.draw)))
+      (show [] (show (reset! stage (Stage.))))
+      (render [delta] (render @stage delta))
       (dispose [])
       (hide [])
       (pause [])
