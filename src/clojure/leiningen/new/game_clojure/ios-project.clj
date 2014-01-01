8dd687951def5808e39efe74edd8740498cad4eb
oakes <zsoakes@gmail.com>
2013-12-31 23:39:29 -0500

    Link play-clj classes in iOS project

--- src/clojure/leiningen/new/game_clojure/ios-project.clj
+++ src/clojure/leiningen/new/game_clojure/ios-project.clj
@@ -9,7 +9,7 @@
   :source-paths ["src/clojure" "../desktop/src-common"]
   :java-source-paths ["src/java"]
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
-  :ios {:robovm-opts ["-forcelinkclasses" "{{package-prefix}}.**:clojure.**:com.badlogic.**"
+  :ios {:robovm-opts ["-forcelinkclasses" "{{package-prefix}}.**:clojure.**:com.badlogic.**:play_clj.**"
                       "-libs" "libs/libObjectAL.a:libs/libgdx.a"
                       "-frameworks" "UIKit:OpenGLES:QuartzCore:CoreGraphics:OpenAL:AudioToolbox:AVFoundation"
                       "-resources" "../desktop/resources/**"]}
