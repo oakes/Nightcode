22c0e40327d25fc5d6b3a7f01c68e90ff0f954d3
oakes <zsoakes@gmail.com>
2014-01-01 16:40:42 -0500

    Update clojure game template to use new entity system

--- src/clojure/leiningen/new/game_clojure/core.clj
+++ src/clojure/leiningen/new/game_clojure/core.clj
@@ -4,22 +4,20 @@
            [com.badlogic.gdx.graphics.g2d BitmapFont]
            [com.badlogic.gdx.scenes.scene2d.ui Label Label$LabelStyle]))
 
-(defn on-show
-  [screen]
-  (create-renderer! screen :type :stage)
-  (let [style (Label$LabelStyle. (BitmapFont.) (Color. 1 1 1 1))
-        label (Label. "Hello world!" style)]
-    (set-entities! screen [label])))
-
-(defn on-render
-  [screen]
-  (clear!)
-  (draw! screen (get-entities screen)))
-
 (defscreen main-screen
-  :on-show on-show
-  :on-render on-render)
+  :on-show
+  (fn [screen entities]
+    (create-renderer! screen :type :stage)
+    (let [style (Label$LabelStyle. (BitmapFont.) (Color. 1 1 1 1))
+          label (Label. "Hello world!" style)]
+      [label]))
+  :on-render
+  (fn [screen entities]
+    (clear!)
+    (draw! screen entities)
+    entities))
 
 (defgame {{app-name}}
-  :on-create (fn [this]
-               (set-screen! this main-screen)))
+  :on-create
+  (fn [this]
+    (set-screen! this main-screen)))
