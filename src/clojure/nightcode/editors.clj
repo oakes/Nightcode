61073c01a3942e9c86e06e4a5b4fe94292dbf13f
Zach Oakes <zsoakes@gmail.com>
2014-02-18 21:42:20 -0500

    Switch shortcuts for selecting tabs and project tree rows

--- src/clojure/nightcode/editors.clj
+++ src/clojure/nightcode/editors.clj
@@ -96,7 +96,7 @@
                    (if (utils/is-parent-path? path e-path) "underline" "none")
                    (if (italicize-fn) "italic" "normal")
                    (-> e-path io/file .getName)))
-         (cons "<center>PgUp PgDn</center>")
+         (cons "<center>&uarr; &darr;</center>")
          (clojure.string/join "<br/>")
          shortcuts/wrap-hint-text
          (s/editor-pane :editable? false :content-type "text/html" :text)
