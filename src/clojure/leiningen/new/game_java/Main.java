a2ca2f53e7b4f7d3e95ff34d94455789b47f5af3
Zach Oakes <zsoakes@gmail.com>
2014-03-07 20:09:57 -0500

    Don't enforce powers of two in the Java game template

--- src/clojure/leiningen/new/game_java/Main.java
+++ src/clojure/leiningen/new/game_java/Main.java
@@ -1,9 +1,11 @@
 package {{package}};
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
 
 public class {{class-name}} extends Game {
 	public void create() {
+		Texture.setEnforcePotImages(false);
 		this.setScreen(new {{screen-class-name}}());
 	}
 }
