16b2474e6059be8d8e217bd80b33ccfbc7358c38
oakes <zsoakes@gmail.com>
2014-03-24 00:10:09 -0400

    Update web template

--- src/clojure/leiningen/new/web_clojure/project.clj
+++ src/clojure/leiningen/new/web_clojure/project.clj
@@ -1,8 +1,8 @@
 (defproject {{name}} "0.0.1-SNAPSHOT"
   :description "FIXME: write this!"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2156"]
-                 [ring "1.2.1"]]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [ring "1.2.2"]]
   :plugins [[lein-cljsbuild "1.0.2"]
             [lein-ring "0.8.10"]]
   :hooks [leiningen.cljsbuild]
