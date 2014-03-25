482a76402b936a4e20d0b3543754c592ac90d320
oakes <zsoakes@gmail.com>
2014-03-25 18:25:59 -0400

    Update examples

--- examples/nightbuild/project.clj
+++ examples/nightbuild/project.clj
@@ -1,7 +1,7 @@
 (defproject nightbuild "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[nightcode "LATEST"]
-                 [org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojure "1.6.0"]
                  [seesaw "1.4.4"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [nightbuild.core]
--- examples/nightpad/project.clj
+++ examples/nightpad/project.clj
@@ -7,7 +7,7 @@
                                lein-droid
                                lein-fruit
                                play-clj/lein-template]]
-                 [org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojure "1.6.0"]
                  [seesaw "1.4.4"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [nightpad.core]
