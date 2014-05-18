c832bb12fbb21dd7c1bd3ac168f60bf368bf3b07
oakes <zsoakes@gmail.com>
2014-05-18 17:09:14 -0400

    Update quil

--- src/clojure/leiningen/new/graphics_clojure/project.clj
+++ src/clojure/leiningen/new/graphics_clojure/project.clj
@@ -2,5 +2,5 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [quil "1.7.0"]]
+                 [quil "2.0.0"]]
   :main {{namespace}})
