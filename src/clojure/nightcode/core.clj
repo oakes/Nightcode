50daa45f7cfbbc728366058840cbcb33d0d7dc65
hellonico <hellonico@gmail.com>
2014-05-26 18:22:28 +0900

    more options to setup window size and panel layout on command line

--- src/clojure/nightcode/core.clj
+++ src/clojure/nightcode/core.clj
@@ -1,6 +1,7 @@
 (ns nightcode.core
   (:require [nightcode.builders :as builders]
             [nightcode.dialogs :as dialogs]
+            [nightcode.cli-args :as cli-args]
             [nightcode.editors :as editors]
             [nightcode.logcat :as logcat]
             [nightcode.projects :as projects]
@@ -15,7 +16,7 @@
 
 (defn create-window-content
   "Returns the entire window with all panes."
-  []
+  [args]
   (let [console (editors/create-console "*repl*")
         one-touch! #(doto % (.setOneTouchExpandable true))]
     (one-touch!
@@ -23,37 +24,50 @@
         (one-touch!
           (s/top-bottom-split (projects/create-pane console)
                               (repl/create-pane console)
-                              :divider-location 0.8
+                              :divider-location 0.7
                               :resize-weight 0.5))
-        (one-touch!
-          (s/top-bottom-split (editors/create-pane)
+          (one-touch!
+            (if (= (args :panel) "horizontal")
+              (s/left-right-split 
+                              (editors/create-pane)
                               (builders/create-pane)
-                              :divider-location 0.8
-                              :resize-weight 0.5))
+                              :divider-location 0.5
+                              :resize-weight 0.5)
+              (s/top-bottom-split 
+                (editors/create-pane)
+                              (builders/create-pane)
+                              :divider-location 0.7
+                              :resize-weight 0.5)))
         :divider-location 0.32
         :resize-weight 0))))
 
 (defn create-window
   "Creates the main window."
-  []
+  [args]
+  (let [
+    screen (.getScreenSize (java.awt.Toolkit/getDefaultToolkit))
+    height (if (args :fullscreen) (.getHeight screen) 768)
+    width  (if (args :fullscreen) (.getWidth screen) 1242)
+      ]
   (doto (s/frame :title (str "Nightcode " (or (some-> "nightcode.core"
                                                       utils/get-project
                                                       (nth 2))
                                               "beta"))
-                 :content (create-window-content)
-                 :width 1242
-                 :height 768
+                 :content (create-window-content args)
+                 :width width
+                 :height height
                  :icon "logo_launcher.png"
                  :on-close :nothing)
     ; set various window properties
     window/enable-full-screen!
-    window/add-listener!))
+    window/add-listener!)))
 
 (defn -main
   "Launches the main window."
   [& args]
+  (let [parsed-args (cli-args/parse-args args)]
   (window/set-icon! "logo_launcher.png")
-  (window/set-theme! args)
+  (window/set-theme! parsed-args)
   (sandbox/set-home!)
   (sandbox/create-profiles-clj!)
   (sandbox/read-file-permissions!)
@@ -79,6 +93,6 @@
           ; else
           false)))
     ; create and show the frame
-    (s/show! (reset! ui/root (create-window)))
+    (s/show! (reset! ui/root (create-window parsed-args)))
     ; initialize the project pane
-    (ui/update-project-tree!)))
+    (ui/update-project-tree!))))
