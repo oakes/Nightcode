8a295113e5c4b64297618f84efe16a9796142119
oakes <zsoakes@gmail.com>
2014-05-29 00:14:26 -0400

    Increment version in examples

--- examples/nightrepl/project.clj
+++ examples/nightrepl/project.clj
@@ -1,6 +1,6 @@
 (defproject nightrepl "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[nightcode "0.3.5"]
+  :dependencies [[nightcode "0.3.6"]
                  [org.clojure/clojure "1.6.0"]
                  [seesaw "1.4.4"]]
   :uberjar-exclusions [#"clojure-clr.*\.zip"]
