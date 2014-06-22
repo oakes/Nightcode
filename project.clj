5ca64fc3eddc71bdece98a27569054fe19fea59a
oakes <zsoakes@gmail.com>
2014-06-22 17:32:55 -0400

    Update libraries

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :dependencies [[com.fifesoft/autocomplete "2.5.0"]
                  [com.fifesoft/rsyntaxtextarea "2.5.0"]
                  [com.github.insubstantial/substance "7.2.1"]
-                 [compliment "0.1.0"]
-                 [leiningen "2.3.4"
+                 [compliment "0.1.1"]
+                 [leiningen "2.4.0"
                   :exclusions [leiningen.search]]
                  [lein-ancient "0.5.4"
                   :exclusions [clj-aws-s3]]
