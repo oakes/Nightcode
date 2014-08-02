f84144c2fa9eb6ea61aa21f7c4f7fde53cb5f933
oakes <zsoakes@gmail.com>
2014-08-01 23:44:05 -0400

    Update libraries in templates

--- src/clojure/leiningen/new/database_clojure/project.clj
+++ src/clojure/leiningen/new/database_clojure/project.clj
@@ -1,8 +1,8 @@
 (defproject {{app-name}} "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[com.h2database/h2 "1.4.178"]
+  :dependencies [[com.h2database/h2 "1.4.180"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/java.jdbc "0.3.3"]]
+                 [org.clojure/java.jdbc "0.3.5"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [{{namespace}}]
   :main {{namespace}})
--- src/clojure/leiningen/new/graphics_clojure/project.clj
+++ src/clojure/leiningen/new/graphics_clojure/project.clj
@@ -2,5 +2,5 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [quil "2.1.0"]]
+                 [quil "2.2.0"]]
   :main {{namespace}})
