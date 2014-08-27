69ce2b8b6398ea24055884b767469bcc55d9f9c3
oakes <zsoakes@gmail.com>
2014-08-27 14:51:19 -0400

    Update version in examples

--- examples/nightpad/project.clj
+++ examples/nightpad/project.clj
@@ -1,6 +1,6 @@
 (defproject nightpad "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[nightcode "0.3.9"
+  :dependencies [[nightcode "0.3.11"
                   :exclusions [leiningen
                                lein-ancient
                                lein-cljsbuild
