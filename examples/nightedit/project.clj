d8f5aa395c93e61731b56b1213e7c6d761dd69a7
oakes <zsoakes@gmail.com>
2014-09-16 12:39:50 -0400

    Update examples

--- examples/nightedit/project.clj
+++ examples/nightedit/project.clj
@@ -1,6 +1,6 @@
 (defproject nightedit "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[nightcode "0.3.11"
+  :dependencies [[nightcode "0.3.12"
                   :exclusions [leiningen
                                lein-ancient
                                lein-cljsbuild
@@ -11,7 +11,6 @@
                                play-clj/lein-template]]
                  [org.clojure/clojure "1.6.0"]
                  [seesaw "1.4.4"]]
-  :uberjar-exclusions [#"clojure-clr.*\.zip"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [nightedit.core]
   :main nightedit.core)
