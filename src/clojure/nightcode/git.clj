2ea6c1b9a7b1928e3597ec8321909bc9209b365d
oakes <zsoakes@gmail.com>
2014-09-22 03:26:04 -0400

    Switch back/forward functionality

--- src/clojure/nightcode/git.clj
+++ src/clojure/nightcode/git.clj
@@ -388,22 +388,22 @@
   ([offset-atom]
     (some-> @ui/root (update-paging-buttons! offset-atom)))
   ([panel offset-atom]
-    (let [back (s/select panel [:#git-back])]
-      (s/config! back :enabled? (> @offset-atom 0)))))
+    (let [forward (s/select panel [:#git-forward])]
+      (s/config! forward :enabled? (> @offset-atom 0)))))
 
 (defn create-paging-buttons
   [offset-atom]
   [(s/button :id :git-back
              :text (shortcuts/wrap-hint-text "&larr;")
              :listen [:action (fn [& _]
-                                (swap! offset-atom - max-commits)
+                                (swap! offset-atom + max-commits)
                                 (update-paging-buttons! offset-atom)
                                 (some-> (update-sidebar!)
                                         (s/scroll! :to :top)))])
    (s/button :id :git-forward
              :text (shortcuts/wrap-hint-text "&rarr;")
              :listen [:action (fn [& _]
-                                (swap! offset-atom + max-commits)
+                                (swap! offset-atom - max-commits)
                                 (update-paging-buttons! offset-atom)
                                 (some-> (update-sidebar!)
                                         (s/scroll! :to :top)))])])
