5c70378d533b410e31ecec8c92820277508991dc
oakes <zsoakes@gmail.com>
2014-10-17 14:31:05 -0400

    Update play-clj template

--- project.clj
+++ project.clj
@@ -28,7 +28,7 @@
                  [org.flatland/ordered "1.5.2"]
                  [org.lpetit/paredit.clj "0.19.3"
                   :exclusions [org.clojure/clojure]]
-                 [play-clj/lein-template "0.4.0"]
+                 [play-clj/lein-template "0.4.1"]
                  [seesaw "1.4.4"]]
   :uberjar-exclusions [#"PHPTokenMaker\.class"
                        #"org\/apache\/lucene"]
