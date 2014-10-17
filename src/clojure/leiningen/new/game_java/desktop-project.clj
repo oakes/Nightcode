51d8a850402a58d8e3b4761f14f526cb7064355b
oakes <zsoakes@gmail.com>
2014-10-17 14:28:20 -0400

    Update java game template

--- src/clojure/leiningen/new/game_java/desktop-project.clj
+++ src/clojure/leiningen/new/game_java/desktop-project.clj
@@ -1,15 +1,15 @@
 (defproject {{app-name}} "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
   
-  :dependencies [[com.badlogicgames.gdx/gdx "1.3.1"]
-                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.3.1"]
-                 [com.badlogicgames.gdx/gdx-box2d "1.3.1"]
-                 [com.badlogicgames.gdx/gdx-box2d-platform "1.3.1"
+  :dependencies [[com.badlogicgames.gdx/gdx "1.4.1"]
+                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.4.1"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.4.1"]
+                 [com.badlogicgames.gdx/gdx-box2d-platform "1.4.1"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-bullet "1.3.1"]
-                 [com.badlogicgames.gdx/gdx-bullet-platform "1.3.1"
+                 [com.badlogicgames.gdx/gdx-bullet "1.4.1"]
+                 [com.badlogicgames.gdx/gdx-bullet-platform "1.4.1"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-platform "1.3.1"
+                 [com.badlogicgames.gdx/gdx-platform "1.4.1"
                   :classifier "natives-desktop"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   
