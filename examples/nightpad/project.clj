ea3ac331e44f3f5bd473f394776bdfc1ad2b305f
oakes <zsoakes@gmail.com>
2014-10-17 15:23:10 -0400

    Update examples

--- examples/nightpad/project.clj
+++ examples/nightpad/project.clj
@@ -1,6 +1,6 @@
 (defproject nightpad "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[nightcode "0.4.0"
+  :dependencies [[nightcode "0.4.1"
                   :exclusions [gwt-plugin
                                leiningen
                                lein-ancient
