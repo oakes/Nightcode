3b4286772b70feb7ad5a5c91cc330fbe087641d2
oakes <zsoakes@gmail.com>
2014-11-05 20:50:20 -0500

    Update libraries in templates

--- src/clojure/leiningen/new/web_clojure/project.clj
+++ src/clojure/leiningen/new/web_clojure/project.clj
@@ -1,10 +1,10 @@
 (defproject {{name}} "0.0.1-SNAPSHOT"
   :description "FIXME: write this!"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2342"]
+                 [org.clojure/clojurescript "0.0-2371"]
                  [ring "1.3.1"]]
   :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-ring "0.8.11"]]
+            [lein-ring "0.8.13"]]
   :hooks [leiningen.cljsbuild]
   :source-paths ["src/clj"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
