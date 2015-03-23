11c32159e0242fb16b22dc2279e449520d7193c4
oakes <zsoakes@gmail.com>
2015-03-22 21:20:41 -0400

    Revert "Improve Quil cljs template"
    This reverts commit 3226b545fa1124acf9888f1a4f3b914752c8db0e.

--- src/clojure/leiningen/new/graphics_clojurescript/client.cljs
+++ src/clojure/leiningen/new/graphics_clojurescript/client.cljs
@@ -1,33 +1,16 @@
-(ns breakout.core
-  (:require [quil.core :as q :include-macros true]
-            [quil.middleware :as m]))
+(ns {{namespace}}
+  (:require [quil.core :as q :include-macros true]))
+
+(defn setup []
+  (q/smooth))
+
+(defn draw []
+  (q/background 255)
+  (q/fill 192)
+  (q/ellipse 100 100 30 30))
 
 (q/defsketch example
   :host "sketch"
-  
-  :size [(.-innerWidth js/window) (.-innerHeight js/window)]
-  
-  :middleware [m/fun-mode]
-  
-  ; create the initial state
-  :setup
-  (fn []
-    {:x 0 :y 0 :r 10})
-  
-  ; draw the state
-  :draw
-  (fn [state]
-    (q/background 255)
-    (q/ellipse (:x state) (:y state) (:r state) (:r state)))
-  
-  ; update the state on each frame
-  :update
-  (fn [state]
-    (update-in state [:r] inc))
-  
-  ; update the state when the mouse is moved
-  :mouse-moved
-  (fn [state event]
-    (-> state
-      (assoc :x (:x event) :y (:y event))
-      (update-in [:r] #(min 10 (dec %))))))
+  :setup setup
+  :draw draw
+  :size [200 200])
