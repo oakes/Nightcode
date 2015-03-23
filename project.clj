10e34db81e468bb252fdc9674551973fbc55b141
oakes <zsoakes@gmail.com>
2015-03-23 00:49:26 -0400

    Start 0.4.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nightcode "0.4.6"
+(defproject nightcode "0.4.7-SNAPSHOT"
   :description "An IDE for Clojure and Java"
   :url "https://github.com/oakes/Nightcode"
   :license {:name "Public Domain"
