1ac3d581cf149093db1ed6f8e13b0b12919e05d3
oakes <zsoakes@gmail.com>
2015-04-15 17:04:01 -0400

    Update phaser

--- src/clojure/leiningen/new/game_javascript.clj
+++ src/clojure/leiningen/new/game_javascript.clj
@@ -14,6 +14,7 @@
                ["README.md" (render "README.md" data)]
                [".gitignore" (render "gitignore" data)]
                ["src/{{path}}.clj" (render "server.clj" data)]
-               ["resources/public/index.html" (render "index.html" data)]
-               ["resources/public/main.js" (render "main.js" data)]
-               ["resources/public/phaser.min.js" (render "phaser.min.js" data)])))
+               ["resources/public/index.html" "index.html"]
+               ["resources/public/main.js" "main.js"]
+               ["resources/public/phaser.js" "phaser.js"]
+               ["resources/public/phaser.min.js" "phaser.min.js"])))
