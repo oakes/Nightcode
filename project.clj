1e847e2d67501a0ec05a17bf048ee8dece0b68c8
oakes <zsoakes@gmail.com>
2015-07-03 19:12:54 -0400

    Update libraries

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/oakes/Nightcode"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[com.fifesoft/autocomplete "2.5.4"]
-                 [com.fifesoft/rsyntaxtextarea "2.5.6"]
+  :dependencies [[com.fifesoft/autocomplete "2.5.7"]
+                 [com.fifesoft/rsyntaxtextarea "2.5.7"]
                  [com.github.insubstantial/substance "7.3"]
-                 [compliment "0.2.2"]
+                 [compliment "0.2.4"]
                  [gwt-plugin "0.1.6"]
                  [hiccup "1.0.5"]
                  [leiningen "2.5.1"
@@ -18,16 +18,19 @@
                  [lein-droid "0.3.5"]
                  [lein-fruit "0.2.2"]
                  [lein-typed "0.3.5"]
+                 [net.cgrand/parsley "0.9.3"
+                  :exclusions [org.clojure/clojure]]
                  [net.java.balloontip/balloontip "1.2.4.1"]
-                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojure "1.7.0"]
                  [org.clojure/core.incubator "0.1.3"]
                  [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/tools.namespace "0.2.10"]
                  [org.eclipse.jgit "3.5.3.201412180710-r"
                   :exclusions [org.apache.httpcomponents/httpclient]]
-                 [org.flatland/ordered "1.5.2"]
+                 [org.flatland/ordered "1.5.3"]
                  [org.lpetit/paredit.clj "0.19.3"
-                  :exclusions [org.clojure/clojure]]
+                  :exclusions [net.cgrand/parsley
+                               org.clojure/clojure]]
                  [play-clj/lein-template "0.4.6"]
                  [seesaw "1.4.5"]]
   :uberjar-exclusions [#"PHPTokenMaker\.class"
