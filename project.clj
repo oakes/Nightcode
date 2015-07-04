7986f37f1f3b6adfaf3f694c09396482c2b1aa56
oakes <zsoakes@gmail.com>
2015-07-04 12:05:34 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nightcode "0.4.7-SNAPSHOT"
+(defproject nightcode "0.4.7"
   :description "An IDE for Clojure and Java"
   :url "https://github.com/oakes/Nightcode"
   :license {:name "Public Domain"
