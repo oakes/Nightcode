ed5559ebad4797ca10e352d091a9e2ecdba3fbbb
oakes <zsoakes@gmail.com>
2015-07-04 12:49:27 -0400

    Update examples

--- examples/nightedit/project.clj
+++ examples/nightedit/project.clj
@@ -1,6 +1,6 @@
 (defproject nightedit "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[nightcode "0.4.6"
+  :dependencies [[nightcode "0.4.7"
                   :exclusions [gwt-plugin
                                leiningen
                                lein-ancient
@@ -10,8 +10,8 @@
                                lein-fruit
                                lein-typed
                                play-clj/lein-template]]
-                 [org.clojure/clojure "1.6.0"]
-                 [seesaw "1.4.4"]]
+                 [org.clojure/clojure "1.7.0"]
+                 [seesaw "1.4.5"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [nightedit.core]
   :main nightedit.core)
--- examples/nightrepl/project.clj
+++ examples/nightrepl/project.clj
@@ -1,8 +1,8 @@
 (defproject nightrepl "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[nightcode "0.4.6"]
-                 [org.clojure/clojure "1.6.0"]
-                 [seesaw "1.4.4"]]
+  :dependencies [[nightcode "0.4.7"]
+                 [org.clojure/clojure "1.7.0"]
+                 [seesaw "1.4.5"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [nightrepl.core]
   :main nightrepl.core)
