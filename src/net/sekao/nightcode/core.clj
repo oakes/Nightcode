7cc67001043eacfcd06e86a66945f0e6b1508629
oakes <zsoakes@gmail.com>
2015-11-20 17:01:28 -0500

    Add fxml and initialization code

--- src/net/sekao/nightcode/core.clj
+++ src/net/sekao/nightcode/core.clj
@@ -1,5 +1,19 @@
 (ns net.sekao.nightcode.core
-  (:gen-class))
+  (:import [javafx.application Application]
+           [javafx.fxml FXMLLoader]
+           [javafx.stage Stage StageBuilder]
+           [javafx.scene Scene])
+  (:gen-class :extends javafx.application.Application))
+
+(defn -start [app ^Stage stage]
+  (let [root (FXMLLoader/load (clojure.java.io/resource "main.fxml"))]
+    (doto stage
+      (.setTitle "Nightcode")
+      (.setScene (Scene. root 800 600))
+      (.show))))
+
+(defn -stop [app]
+  (println "Exiting"))
 
 (defn -main [& args]
-  (println "Hello, world!"))
+  (Application/launch net.sekao.nightcode.core (into-array String args)))
