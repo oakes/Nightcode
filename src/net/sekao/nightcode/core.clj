030a259d4168193a807fbe2ce16655d9a137ff14
oakes <zsoakes@gmail.com>
2015-11-21 14:12:09 -0500

    Load editor in web view

--- src/net/sekao/nightcode/core.clj
+++ src/net/sekao/nightcode/core.clj
@@ -1,4 +1,5 @@
 (ns net.sekao.nightcode.core
+  (:require [clojure.java.io :refer [resource]])
   (:import [javafx.application Application]
            [javafx.fxml FXMLLoader]
            [javafx.stage Stage StageBuilder]
@@ -6,11 +7,15 @@
   (:gen-class :extends javafx.application.Application))
 
 (defn -start [app ^Stage stage]
-  (let [root (FXMLLoader/load (clojure.java.io/resource "main.fxml"))]
+  (let [root (FXMLLoader/load (clojure.java.io/resource "main.fxml"))
+        scene (Scene. root 800 600)
+        editor (.lookup scene "#editor")
+        engine (.getEngine editor)]
     (doto stage
       (.setTitle "Nightcode")
-      (.setScene (Scene. root 800 600))
-      (.show))))
+      (.setScene scene)
+      (.show))
+    (.load engine (.toExternalForm (resource "public/index.html")))))
 
 (defn -main [& args]
   (Application/launch net.sekao.nightcode.core (into-array String args)))
