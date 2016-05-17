3ab7ffaf939cef07b2309310df0b360c746256c1
oakes <zsoakes@gmail.com>
2016-05-16 21:56:18 -0400

    Update examples

--- examples/nightrepl/project.clj
+++ examples/nightrepl/project.clj
@@ -1,6 +1,6 @@
 (defproject nightrepl "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[nightcode "1.2.1"]
+  :dependencies [[nightcode "1.3.0"]
                  [org.clojure/clojure "1.8.0"]
                  [seesaw "1.4.5"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
