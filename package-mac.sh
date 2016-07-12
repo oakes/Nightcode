cf7d09f48c58895b88d2c8c1a7493ec95618fe32
oakes <zsoakes@gmail.com>
2016-07-11 20:45:21 -0400

    Use full path to javapackager

---
+++ package-mac.sh
@@ -0,0 +1,1 @@
+/Library/Java/Home/bin/javapackager -deploy -native -outdir package -outfile Nightcode -srcdir . -srcfiles target/project.jar -appclass net.sekao.nightcode.core -name "Nightcode" -title "Nightcode"
--- package.sh
+++
@@ -1,1 +0,0 @@
-javapackager -deploy -native -outdir package -outfile Nightcode -srcdir . -srcfiles target/project.jar -appclass net.sekao.nightcode.core -name "Nightcode" -title "Nightcode"
