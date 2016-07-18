26975ce7108e8464a3adae1981e8e7263e568fe2
oakes <zsoakes@gmail.com>
2016-07-18 00:47:22 -0400

    Decrement version number

--- src/net/sekao/nightcode/core.clj
+++ src/net/sekao/nightcode/core.clj
@@ -39,7 +39,7 @@
         content (.lookup scene "#content")]
     (swap! runtime-state assoc :stage stage)
     (doto stage
-      (.setTitle "Nightcode 2.1.0")
+      (.setTitle "Nightcode 2.0.1")
       (.setScene scene)
       (.show))
     (shortcuts/init-tabs! scene)
