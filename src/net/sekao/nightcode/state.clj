f7e9d3130140e88b0f51ceed37c20f6100dd4b4d
oakes <zsoakes@gmail.com>
2016-09-26 20:33:53 -0400

    Bump default text size

--- src/net/sekao/nightcode/state.clj
+++ src/net/sekao/nightcode/state.clj
@@ -36,7 +36,7 @@
                            :expansion-set (read-pref :expansion-set #{})
                            :selection (read-pref :selection)
                            :theme (read-pref :theme :dark)
-                           :text-size (read-pref :text-size 15)
+                           :text-size (read-pref :text-size 16)
                            :auto-save? (read-pref :auto-save? true)}))
 
 (defonce runtime-state (atom {:web-port nil
