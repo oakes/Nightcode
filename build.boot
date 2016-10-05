8ea0d782928a21c1f9c57b6390d5c7346a7b8d17
oakes <zsoakes@gmail.com>
2016-10-05 16:41:14 -0400

    Start 2.1.6-SNAPSHOT

--- build.boot
+++ build.boot
@@ -18,7 +18,7 @@
 (task-options!
   sift {:include #{#"\.jar$"}}
   pom {:project 'nightcode
-       :version "2.1.5"}
+       :version "2.1.6-SNAPSHOT"}
   aot {:namespace '#{nightcode.core
                      nightcode.lein}}
   jar {:main 'nightcode.core
--- src/clj/nightcode/core.clj
+++ src/clj/nightcode/core.clj
@@ -39,7 +39,7 @@
         content (.lookup scene "#content")]
     (swap! runtime-state assoc :stage stage)
     (doto stage
-      (.setTitle "Nightcode 2.1.5")
+      (.setTitle "Nightcode 2.1.6-SNAPSHOT")
       (.setScene scene)
       (.show))
     (shortcuts/init-tabs! scene)
