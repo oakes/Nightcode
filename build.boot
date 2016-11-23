ff8ddc7326a5e4c18fba105d38c475ccfbbcc67c
oakes <zsoakes@gmail.com>
2016-11-23 10:09:02 -0500

    Increment version number

--- build.boot
+++ build.boot
@@ -19,7 +19,7 @@
 (task-options!
   sift {:include #{#"\.jar$"}}
   pom {:project 'nightcode
-       :version "2.1.7-SNAPSHOT"}
+       :version "2.1.7"}
   aot {:namespace '#{nightcode.core
                      nightcode.lein}}
   jar {:main 'nightcode.core
--- src/clj/nightcode/core.clj
+++ src/clj/nightcode/core.clj
@@ -39,7 +39,7 @@
         content (.lookup scene "#content")]
     (swap! runtime-state assoc :stage stage)
     (doto stage
-      (.setTitle "Nightcode 2.1.7-SNAPSHOT")
+      (.setTitle "Nightcode 2.1.7")
       (.setScene scene)
       (.show))
     (shortcuts/init-tabs! scene)
