a8f6c7750ce0352fd06d0b8823fb90b2e243f409
oakes <zsoakes@gmail.com>
2017-02-28 14:35:08 +0100

    Increment version number

--- src/clj/nightcode/core.clj
+++ src/clj/nightcode/core.clj
@@ -40,7 +40,7 @@
         content (.lookup scene "#content")]
     (swap! runtime-state assoc :stage stage)
     (doto stage
-      (.setTitle "Nightcode 2.3.1-SNAPSHOT")
+      (.setTitle "Nightcode 2.3.1")
       (.setScene scene)
       (.show))
     (shortcuts/init-tabs! scene)
