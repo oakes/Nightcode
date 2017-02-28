aa672c00d5b5235a1aee68c794387c9c78e16959
oakes <zsoakes@gmail.com>
2017-02-28 15:07:32 +0100

    Start 2.3.2-SNAPSHOT

--- src/clj/nightcode/core.clj
+++ src/clj/nightcode/core.clj
@@ -40,7 +40,7 @@
         content (.lookup scene "#content")]
     (swap! runtime-state assoc :stage stage)
     (doto stage
-      (.setTitle "Nightcode 2.3.1")
+      (.setTitle "Nightcode 2.3.2-SNAPSHOT")
       (.setScene scene)
       (.show))
     (shortcuts/init-tabs! scene)
