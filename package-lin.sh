b66d23afb878cc8fd4b3de65a06d8bc7d2301d0b
oakes <zach@sekao.net>
2017-09-30 19:13:03 -0400

    Revert linux script for now

--- package-lin.sh
+++ package-lin.sh
@@ -1,2 +1,1 @@
-javapackager -deploy -native rpm -outdir package -outfile Nightcode -srcdir target -appclass nightcode.core -name "Nightcode" -title "Nightcode" -Bicon=package/linux/Nightcode.png -BappVersion=$1
-javapackager -deploy -native deb -outdir package -outfile Nightcode -srcdir target -appclass nightcode.core -name "Nightcode" -title "Nightcode" -Bicon=package/linux/Nightcode.png -BappVersion=$1
+javapackager -deploy -native -outdir package -outfile Nightcode -srcdir . -srcfiles target/project.jar -appclass nightcode.core -name "Nightcode" -title "Nightcode" -BappVersion=$1
