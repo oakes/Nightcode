de5b9cd8b699d1b0a5b37fac7b750748530852a3
oakes <zach@sekao.net>
2019-01-21 08:47:29 -0600

    Add nightcode.start

---
+++ src/clj/nightcode/start.clj
@@ -0,0 +1,8 @@
+(ns nightcode.start
+  "This ns is a workaround for an issue that prevented making uberjars
+   with JavaFX 11. See https://stackoverflow.com/a/52571719"
+  (:require [nightcode.core :as c])
+  (:gen-class))
+
+(defn -main [& args]
+  (apply c/-main args))
