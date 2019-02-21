b7b8d5e8f48037b1bb1b48e360a78416ee64095f
oakes <zach@sekao.net>
2019-02-21 14:06:04 -0600

    Move system exit

--- prod.clj
+++ prod.clj
@@ -49,8 +49,8 @@
                               [org.openjfx/javafx-web "11.0.2" :classifier "mac"]])
                     p/init-project)]
     (clean project)
-    (uberjar project)
-    (System/exit 0)))
+    (uberjar project))
+  (System/exit 0))
 
 (defmethod task "install"
   [_]
