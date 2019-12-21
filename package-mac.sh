5f99425ba3a6f7ed84e3d1f3057073670df0f676
oakes <zach@sekao.net>
2019-12-21 11:52:42 -0500

    Remove mac package script

--- package-mac.sh
+++
@@ -1,1 +0,0 @@
-javapackager -deploy -native dmg -outdir package -outfile Nightcode -srcdir target -appclass nightcode.core -name "Nightcode" -title "Nightcode" -Bicon=package/macosx/Nightcode.icns -BappVersion=$1
