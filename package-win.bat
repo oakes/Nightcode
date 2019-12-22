c336b968e110d81df70607389908ac7acb69a634
oakes <zach@sekao.net>
2019-12-22 13:19:05 -0500

    Update windows package script

--- package-win.bat
+++ package-win.bat
@@ -1,1 +1,3 @@
+jpackage --type app-image --name Nightcode --app-version %1 --input target --main-jar Nightcode-%1-windows.jar --icon package/windows/Nightcode.ico --copyright "Public Domain"
+
 jpackage --type exe --name Nightcode --app-version %1 --input target --main-jar Nightcode-%1-windows.jar --icon package/windows/Nightcode.ico --copyright "Public Domain" --win-menu --win-per-user-install
